344367b8a69bf2435fe449aa408d4bc70b2288db
Luca Fabri <luca.fabri1999@gmail.com>
2023-12-24 22:05:17 +0100

    chore: setup structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="replay-buffer-service",
+    description="Replay buffer service",
+    url="#",
+    author="Luca Fabri",
+    author_email="luca.fabri1999@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    test_suite="src.test",
+    install_requires=requirements,
+    zip_safe=False,
+    python_requires="==3.11",
+)
