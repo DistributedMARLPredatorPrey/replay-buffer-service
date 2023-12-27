3e5b3238d4242430f18aaec56e6ec1906564a747
Luca Fabri <luca.fabri1999@gmail.com>
2023-12-27 09:15:50 +0100

    chore: format

--- src/test/test_flask_app.py
+++ src/test/test_flask_app.py
@@ -9,33 +9,33 @@ from flask import Flask
 class TestFlaskApp(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__)
-        self.base_url = 'http://127.0.0.1:5000/'
+        self.base_url = "http://127.0.0.1:5000/"
 
-        @self.app.route('/')
+        @self.app.route("/")
         def hello():
-            return 'Hello, World!'
+            return "Hello, World!"
 
         @self.app.route("/shutdown")
         def shutdown():
             print("shutting down")
             raise RuntimeError()
 
-        self.server = Process(target=self.app.run, kwargs={'debug': False})
+        self.server = Process(target=self.app.run, kwargs={"debug": False})
         self.server.start()
         time.sleep(1)  # Give the server time to start
 
     def test_hello_endpoint(self):
         response = requests.get(self.base_url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.text, 'Hello, World!')
+        self.assertEqual(response.text, "Hello, World!")
 
     def tearDown(self):
         self.app.quit = True  # Signal the app to stop
-        #requests.get(f"{self.base_url}shutdown")
+        # requests.get(f"{self.base_url}shutdown")
         self.server.terminate()
         self.server.join()
         # self.server.join()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
