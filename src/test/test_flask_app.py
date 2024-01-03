fcd74bd29580b10323ac9ec521b84b385af2124c
Luca Fabri <luca.fabri1999@gmail.com>
2024-01-03 22:46:00 +0100

    chore: move app to another source

--- src/test/test_flask_app.py
+++ src/test/test_flask_app.py
@@ -3,33 +3,8 @@ from multiprocessing import Process
 from time import sleep
 
 import requests
-from flask import request, jsonify, Flask
 
-# from src.main.simple_flask_router import app
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Hello, World!"
-
-
-@app.route("/batch_data/<size>")
-def batch_data():
-    return "Batch data"
-
-
-@app.route("/record_data", methods=["POST"])
-def record_data():
-    if request.method == "POST":
-        data = request.get_json()
-        print(data)
-        return jsonify({"message": "JSON received successfully"})
-
-
-def run_flask_app():
-    app.run()
+from src.main.simple_flask_router import run_flask_app
 
 
 class TestFlaskApp(unittest.TestCase):
