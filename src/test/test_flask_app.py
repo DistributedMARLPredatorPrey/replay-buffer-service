2fdc1a1b003508b5b8451a1f08905e691ff7613a
Luca Fabri <luca.fabri1999@gmail.com>
2024-01-03 21:54:34 +0100

    chore: run flask app in separate method

--- src/test/test_flask_app.py
+++ src/test/test_flask_app.py
@@ -1,4 +1,3 @@
-import time
 import unittest
 from multiprocessing import Process
 
@@ -6,26 +5,17 @@ import requests
 from flask import Flask
 
 
-def hello():
-    return "Hello, World!"
-
-
-def shutdown():
-    print("shutting down")
-    raise RuntimeError()
-
-
 class TestFlaskApp(unittest.TestCase):
+    app = Flask(__name__)
+
+    def run_flask_app(self):
+        self.app.route("/")(lambda: "Hello, World!")
+        self.app.run()
+
     def setUp(self):
-        self.app = Flask(__name__)
         self.base_url = "http://127.0.0.1:5000/"
-
-        self.app.route("/")(hello)
-        self.app.route("/shutdown")(shutdown)
-
-        self.server = Process(target=self.app.run, kwargs={"debug": False})
+        self.server = Process(target=self.run_flask_app)
         self.server.start()
-        time.sleep(1)  # Give the server time to start
 
     def test_hello_endpoint(self):
         response = requests.get(self.base_url)
@@ -33,12 +23,5 @@ class TestFlaskApp(unittest.TestCase):
         self.assertEqual(response.text, "Hello, World!")
 
     def tearDown(self):
-        self.app.quit = True  # Signal the app to stop
-        # requests.get(f"{self.base_url}shutdown")
         self.server.terminate()
         self.server.join()
-        # self.server.join()
-
-
-if __name__ == "__main__":
-    unittest.main()
