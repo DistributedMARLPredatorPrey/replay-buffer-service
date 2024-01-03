8c23ac17f7488d1509809e4b2bef59957022a201
Luca Fabri <luca.fabri1999@gmail.com>
2024-01-03 22:02:50 +0100

    chore: add sleep, static method

--- src/test/test_flask_app.py
+++ src/test/test_flask_app.py
@@ -1,24 +1,26 @@
 import unittest
 from multiprocessing import Process
+from time import sleep
 
 import requests
 from flask import Flask
 
 
+def run_flask_app(app):
+    app.route("/")(lambda: "Hello, World!")
+    app.run()
+
+
 class TestFlaskApp(unittest.TestCase):
     # app = Flask(__name__)
-
-    @staticmethod
-    def run_flask_app(app):
-        app.route("/")(lambda: "Hello, World!")
-        app.run()
 
     def setUp(self):
         app = Flask(__name__)
         self.base_url = "http://127.0.0.1:5000/"
-        server = Process(target=self.run_flask_app, args=[app])
+        server = Process(target=run_flask_app, args=[app])
         server.start()
         self.server = server
+        sleep(1)
 
     def test_hello_endpoint(self):
         response = requests.get(self.base_url)
