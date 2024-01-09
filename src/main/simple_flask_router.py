7f4f345a735f62ea00fea119f82cdd3ea0a307b3
Luca Fabri <luca.fabri1999@gmail.com>
2024-01-09 16:55:43 +0100

    chore: add buffer service

--- src/main/simple_flask_router.py
+++ src/main/simple_flask_router.py
@@ -2,31 +2,36 @@ from flask import request, Flask
 import pandas as pd
 import os
 
-app = Flask(__name__)
 
+class BufferService:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self._add_rules()
 
-@app.route("/batch_data/<size>")
-def batch_data():
-    return "Batch data"
+    def _add_rules(self):
+        self.app.add_url_rule("/batch_data/<size>", "batch data", self._batch_data)
+        self.app.add_url_rule(
+            "/record_data", "record data", self._record_data, methods=["POST"]
+        )
 
+    @staticmethod
+    def _batch_data():
+        return "Batch data"
 
-@app.route("/record_data", methods=["POST"])
-def record_data():
-    if request.method == "POST":
-        data = request.get_json()
-        file_path = f"{os.path.dirname(os.path.abspath(__file__))}/resources/data.csv"
+    @staticmethod
+    def _record_data():
+        if request.method == "POST":
+            data = request.get_json()
+            file_path = (
+                f"{os.path.dirname(os.path.abspath(__file__))}/resources/data.csv"
+            )
 
-        header = ["State", "Reward", "Action", "Next state"]
-        df = pd.DataFrame(columns=header, data=data)
+            header = ["State", "Reward", "Action", "Next state"]
+            df = pd.DataFrame(columns=header, data=data)
 
-        if os.path.exists(file_path):
-            df = pd.read_csv(file_path)
-            df = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
+            if os.path.exists(file_path):
+                df = pd.read_csv(file_path)
+                df = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
 
-        df.to_csv(file_path, index=False)
-        return "OK"
-
-
-def run_flask_app(**kwargs):
-    print(f"Running Flask Server {kwargs}")
-    app.run(**kwargs)
+            df.to_csv(file_path, index=False)
+            return "OK"
--- src/test/test_flask_app.py
+++ src/test/test_flask_app.py
@@ -1,11 +1,12 @@
 import json
 import unittest
 
-
-from src.main.simple_flask_router import app
+from src.main.simple_flask_router import BufferService
 
 
 class TestFlaskApp(unittest.TestCase):
+    buffer_service = BufferService()
+
     _data = {
         "State": [1.0, 1.1],
         "Reward": [-1, -1],
@@ -14,11 +15,12 @@ class TestFlaskApp(unittest.TestCase):
     }
 
     def setUp(self):
-        app.config["TESTING"] = True
-        self.app = app.test_client()
+        test_app = self.buffer_service.app
+        test_app.config["TESTING"] = True
+        self.client = test_app.test_client()
 
     def test_record_data(self):
-        response = self.app.post(
+        response = self.client.post(
             "record_data", content_type="application/json", data=json.dumps(self._data)
         )
         self.assertEqual(response.status_code, 200)
