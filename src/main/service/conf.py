74f01f2077b107dca20e61a10df0b914c91ddea9
Luca Fabri <luca.fabri1999@gmail.com>
2024-01-13 09:31:10 +0100

    doc: add doc, do some name refactoring

--- src/main/service/conf.py
+++ src/main/service/conf.py
@@ -1,3 +1,7 @@
 import os
+from typing import Dict
 
-REPLAY_BUFFER_PATH = f"{os.path.dirname(os.path.abspath(__file__))}/resources/data.csv"
+# Config data
+conf: Dict[str, str] = {
+    "REPLAY_BUFFER_PATH": f"{os.path.dirname(os.path.abspath(__file__))}/resources/data.csv"
+}
--- src/test/test_buffer_service.py
+++ src/test/test_buffer_service.py
@@ -1,36 +1,53 @@
 import json
 import unittest
+from typing import Dict, List
+
 import pandas as pd
+from flask.testing import FlaskClient
+from werkzeug.test import TestResponse
 
 from src.main.service.response import Response
 from src.main.service.replay_buffer_service import ReplayBufferService
 
 
 class TestReplayBufferService(unittest.TestCase):
-    replay_buffer_service = ReplayBufferService()
+    _replay_buffer_service: ReplayBufferService = ReplayBufferService()
 
-    _data = {
+    _data: Dict[str, List[float]] = {
         "State": [1.0, 1.1],
         "Reward": [-1, -1],
         "Action": [3, 4],
         "Next state": [1.1, 1.2],
     }
 
-    def _post_data(self, data):
+    def _post_data(self, data) -> TestResponse:
+        """
+        Post data using the Replay buffer testing client
+        :param data: to be sent
+        :return:
+        """
         return self.client.post(
             "record_data", content_type="application/json", data=json.dumps(data)
         )
 
     def setUp(self):
-        self.client = self.replay_buffer_service.test_client()
+        self.client: FlaskClient = self._replay_buffer_service.test_client()
 
     def test_record_data(self):
-        response = self._post_data(self._data)
+        """
+        Test the data to be properly recorded into the Replay buffer
+        :return:
+        """
+        response: TestResponse = self._post_data(self._data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, Response.SUCCESSFUL.name)
 
     def test_batch_data(self):
-        batch_size = 2
+        """
+        Test the data to be properly batched from the Replay buffer
+        :return:
+        """
+        batch_size: int = 2
         self._post_data(self._data)
-        data = self.client.get(f"batch_data/{batch_size}").text
+        data: str = self.client.get(f"batch_data/{batch_size}").text
         self.assertEqual(pd.DataFrame(json.loads(data)).shape[0], batch_size)
