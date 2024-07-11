c60586f8072e020da30d74d5f2cc08fc6ca60e4f
Luca Fabri <luca.fabri1999@gmail.com>
2024-07-11 17:21:53 +0200

    refactor: move methods, rename fields

--- src/main/service/replay_buffer_service.py
+++ src/main/service/replay_buffer_service.py
@@ -16,63 +16,83 @@ class ReplayBufferService:
         """
         Init the Replay buffer service by set upping the dataset and by registering the routes.
         """
-        self._app: Flask = Flask(__name__)
-        self._predator_dataset_path: str = predator_dataset_path
-        self._prey_dataset_path: str = prey_dataset_path
-        self._add_rules()
-        self._setup_buffers()
+        self.__app: Flask = Flask(__name__)
+        self.__predator_dataset_path: str = predator_dataset_path
+        self.__prey_dataset_path: str = prey_dataset_path
+        self.__add_rules()
+        self.__setup_buffers()
 
-    def _add_rules(self):
+    def app(self) -> Flask:
+        """
+        Get the app.
+        :return: Flask app of the Replay buffer service
+        """
+        return self.__app
+
+    def test_client(self) -> FlaskClient:
+        """
+        Get the testing client.
+        :return: test client from the Replay buffer service's Flask app
+        """
+        self.__app.config["TESTING"] = True
+        return self.__app.test_client()
+
+    def __add_rules(self):
         """
         Add routing rules to the Replay buffer service.
         :return:
         """
-        self._app.add_url_rule(
-            "/batch_data/<agent_type>/<size>", "batch data", self._batch_data
+        self.__app.add_url_rule(
+            "/batch_data/<agent_type>/<size>", "batch data", self.__batch_data
         )
-        self._app.add_url_rule(
+        self.__app.add_url_rule(
             "/record_data/<agent_type>/",
             "record data",
-            self._record_data,
+            self.__record_data,
             methods=["POST"],
         )
 
-    def _setup_buffers(self):
+    def __setup_buffers(self):
         """
         Set up the buffer, creating it if it does not exist.
         :return:
         """
-        for dataset_path in [self._predator_dataset_path, self._prey_dataset_path]:
+        for dataset_path in [self.__predator_dataset_path, self.__prey_dataset_path]:
             if not os.path.exists(dataset_path):
                 header: List[str] = ["State", "Reward", "Action", "Next state"]
                 df: DataFrame = pd.DataFrame(columns=header)
                 df.to_csv(dataset_path, index=False)
 
-    def _batch_data(self, agent_type, size) -> str:
+    def __batch_data(self, agent_type, size) -> str:
         """
         Batches data from the replay buffer.
         :param size: number of rows to batch
         :return: json representing the data batch
         """
-        dataset_path = self._dataset_path_by_agent_type(agent_type)
-        df = pd.read_csv(dataset_path) if dataset_path is not None else pd.DataFrame()
-        return df.sample(int(size)).to_json() if len(df) >= int(size) else "{}"
+        dataset_path = self.__dataset_path_by_agent_type(agent_type)
+        if dataset_path is not None:
+            if os.path.exists(dataset_path):
+                df = pd.read_csv(dataset_path)
+            else:
+                df = pd.DataFrame()
+            return df.sample(int(size)).to_json() if len(df) >= int(size) else "{}"
+        return "{}"
 
-    def _dataset_path_by_agent_type(self, agent_type):
+    def __dataset_path_by_agent_type(self, agent_type):
         """
         Get the dataset path from the agent type
         :param agent_type: either "predator" or "prey"
         :return: predator or prey dataset path, if agent_type exist. None otherwise.
         """
         return (
-            self._predator_dataset_path
+            self.__predator_dataset_path
             if agent_type == "predator"
-            else self._prey_dataset_path
+            else self.__prey_dataset_path
             if agent_type == "prey"
             else None
         )
 
-    def _record_data(self, agent_type) -> str:
+    def __record_data(self, agent_type) -> str:
         """
         Records data inside the replay buffer.
         :return: Name of the Response:
@@ -82,26 +102,9 @@ class ReplayBufferService:
         """
         if request.method == "POST":
             row = dict(json.loads(request.get_json()))
-            dataset_path = self._dataset_path_by_agent_type(agent_type)
-            # data_df: DataFrame = pd.read_json(StringIO(request.get_json()))
+            dataset_path = self.__dataset_path_by_agent_type(agent_type)
             df: DataFrame = pd.read_csv(dataset_path)
             df = pd.concat([df, pd.DataFrame(row)], ignore_index=True)
-            # df: DataFrame = df.append(row, ignore_index=True)
             df.to_csv(dataset_path, index=False)
             return Response.SUCCESSFUL.name
         return Response.ERROR.name
-
-    def app(self) -> Flask:
-        """
-        Get the app.
-        :return: Flask app of the Replay buffer service
-        """
-        return self._app
-
-    def test_client(self) -> FlaskClient:
-        """
-        Get the testing client.
-        :return: test client from the Replay buffer service's Flask app
-        """
-        self._app.config["TESTING"] = True
-        return self._app.test_client()
