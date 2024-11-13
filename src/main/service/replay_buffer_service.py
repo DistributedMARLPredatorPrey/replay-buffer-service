ffa8d30b885490e893fa400c4f38dffff18e7922
Luca Fabri <luca.fabri1999@gmail.com>
2024-11-13 11:26:25 +0100

    chore: add json schema

--- src/main/service/replay_buffer_service.py
+++ src/main/service/replay_buffer_service.py
@@ -1,22 +1,75 @@
 import json
 import os
-from typing import List
+from enum import Enum
+from typing import List, Callable
 
 import pandas as pd
 from flask import request, Flask
 from flask.testing import FlaskClient
 from pandas import DataFrame
+from jsonschema import validate, ValidationError
+import yaml
+
+from src.main.parser.config import ReplayBufferConfig
+from src.main.service.post_response import PostResponseStatus
+
+
+def schema(num_preds, num_preys, num_states, num_actions):
+    state_schema = {
+        "type": "array",
+        "items": {
+            "type": "array",
+            "minItems": num_states * (num_preds + num_preys),
+            "maxItems": num_states * (num_preds + num_preys),
+            "items": {"type": "number"},
+        },
+    }
+    return {
+        "type": "object",
+        "properties": {
+            "State": state_schema,
+            "Reward": {
+                "type": "array",
+                "items": {
+                    "type": "array",
+                    "minItems": num_preds + num_preys,
+                    "maxItems": num_preds + num_preys,
+                    "items": {"type": "number"},
+                },
+            },
+            "Action": {
+                "type": "array",
+                "items": {
+                    "type": "array",
+                    "minItems": num_actions * (num_preds + num_preys),
+                    "maxItems": num_actions * (num_preds + num_preys),
+                    "items": {"type": "number"},
+                },
+            },
+            "Next State": state_schema,
+        },
+    }
 
 
 class ReplayBufferService:
-    def __init__(self, dataset_path: str):
+    def __init__(self, config: ReplayBufferConfig):
         """
         Init the Replay buffer service by set upping the dataset and by registering the routes.
         """
         self.__app: Flask = Flask(__name__)
-        self.__dataset_path: str = dataset_path
+        self.__dataset_path: str = config.dataset_path
         self.__add_rules()
         self.__setup_buffers()
+        self.__data_record_schema = schema(
+            config.num_predators,
+            config.num_preys,
+            config.num_states,
+            config.num_actions,
+        )
+
+    def __schemas(self):
+        with open("/schemas.yaml", "r") as schema_file:
+            return yaml.safe_load(schema_file)
 
     def app(self) -> Flask:
         """
@@ -58,35 +111,23 @@ class ReplayBufferService:
         df: DataFrame = pd.DataFrame(columns=header)
         df.to_csv(self.__dataset_path, index=False)
 
-    def __batch_data(self, size) -> str:
+    def __batch_data(self, size):
         """
         Batches data from the replay buffer.
         :param size: number of rows to batch
         :return: json representing the data batch
         """
+        empty_json_str: str = "{}"
         if self.__dataset_path is not None:
-            if os.path.exists(self.__dataset_path):
-                df = pd.read_csv(self.__dataset_path)
-            else:
-                df = pd.DataFrame()
-            return df.sample(int(size)).to_json() if len(df) >= int(size) else "{}"
-        return "{}"
+            df: DataFrame = pd.read_csv(self.__dataset_path)
+            return (
+                df.sample(int(size)).to_json()
+                if len(df) >= int(size)
+                else empty_json_str
+            )
+        return empty_json_str
 
-    # def __dataset_path_by_agent_type(self, agent_type):
-    #     """
-    #     Get the dataset path from the agent type
-    #     :param agent_type: either "predator" or "prey"
-    #     :return: predator or prey dataset path, if agent_type exist. None otherwise.
-    #     """
-    #     return (
-    #         self.__predator_dataset_path
-    #         if agent_type == "predator"
-    #         else self.__prey_dataset_path
-    #         if agent_type == "prey"
-    #         else None
-    #     )
-
-    def __record_data(self) -> str:
+    def __record_data(self):
         """
         Records data inside the replay buffer.
         :return: Name of the Response:
@@ -94,10 +135,17 @@ class ReplayBufferService:
             - WRONG_SHAPE if the provided data has a different shape of the replay buffer
             - ERROR if a generic error occurred
         """
-        if request.method == "POST":
-            row = dict(json.loads(request.get_json()))
+        response: Callable[[PostResponseStatus], str] = lambda status: json.dumps(
+            {"status": status.value}
+        )
+        row = request.json
+        try:
+            validate(instance=row, schema=self.__data_record_schema)
             df: DataFrame = pd.read_csv(self.__dataset_path)
             df = pd.concat([df, pd.DataFrame(row)], ignore_index=True)
             df.to_csv(self.__dataset_path, index=False)
-            return "OK"
-        return "ERROR"
+            return response(PostResponseStatus.OK)
+        except ValidationError:
+            return response(PostResponseStatus.INVALID_SCHEMA)
+        except:
+            return response(PostResponseStatus.INTERNAL_ERROR)
--- src/test/test_buffer_service.py
+++ src/test/test_buffer_service.py
@@ -7,21 +7,29 @@ import pandas as pd
 from flask.testing import FlaskClient
 from werkzeug.test import TestResponse
 
-from src.main.service.response import Response
+from src.main.parser.config import ReplayBufferConfig
+from src.main.service.post_response import PostResponseStatus
 from src.main.service.replay_buffer_service import ReplayBufferService
 
 
 class TestReplayBufferService(unittest.TestCase):
     __replay_buffer_service: ReplayBufferService = ReplayBufferService(
-        predator_dataset_path=f"{os.path.dirname(os.path.abspath(__file__))}/resources/predator.csv",
-        prey_dataset_path=f"{os.path.dirname(os.path.abspath(__file__))}/resources/prey.csv",
+        ReplayBufferConfig(
+            num_predators=1,
+            num_preys=1,
+            num_states=2,
+            num_actions=2,
+            dataset_path=os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "resources", "dataset.csv"
+            ),
+        )
     )
 
     __data: Dict[str, List[float]] = {
         "State": [[1.0, 1.0, 2.0, 2.0], [1.1, 1.1, 2.1, 2.1]],
         "Reward": [[1, 2], [1, 2]],
         "Action": [[1.0, 1.0, 2.0, 2.0], [1.1, 1.1, 2.1, 2.1]],
-        "Next state": [[1.0, 1.0, 2.0, 2.0], [1.1, 1.1, 2.1, 2.1]],
+        "Next State": [[1.0, 1.0, 2.0, 2.0], [1.1, 1.1, 2.1, 2.1]],
     }
 
     def __post_data(self, data) -> TestResponse:
@@ -31,30 +39,50 @@ class TestReplayBufferService(unittest.TestCase):
         :return:
         """
         return self.client.post(
-            "record_data/predator/",
-            content_type="application/json",
-            data=json.dumps(data),
+            "record_data/",
+            json=data,
         )
 
     def setUp(self):
         self.client: FlaskClient = self.__replay_buffer_service.test_client()
 
-    def test_record_data(self):
+    def test_record_data_ok(self):
         """
         Test the data to be properly recorded into the Replay buffer
         :return:
         """
         response: TestResponse = self.__post_data(self.__data)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.text, Response.SUCCESSFUL.name)
+        self.assertEqual(
+            response.text, json.dumps({"status": PostResponseStatus.OK.value})
+        )
 
-    def test_batch_data(self):
+    def test_record_data_wrong_schema(self):
         """
-        Test the data to be properly batched from the Replay buffer
+        Test the data to be properly recorded into the Replay buffer
         :return:
         """
-        batch_size: int = 2
-        for _ in range(100):
-            self.__post_data(self.__data)
-        data: str = self.client.get(f"batch_data/predator/{batch_size}").text
-        self.assertEqual(pd.DataFrame(json.loads(data)).shape[0], batch_size)
+        invalid_data =  {
+            "State": [[1.0, 1.0], [1.1, 1.1, 2.1, 2.1]],
+            "Reward": self.__data["Reward"],
+            "Action": self.__data["Action"],
+            "Next State": self.__data["Next State"]
+        }
+
+        response: TestResponse = self.__post_data(invalid_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.text, json.dumps({"status": PostResponseStatus.INVALID_SCHEMA.value})
+        )
+
+
+    # def test_batch_data(self):
+    #     """
+    #     Test the data to be properly batched from the Replay buffer
+    #     :return:
+    #     """
+    #     batch_size: int = 2
+    #     for _ in range(100):
+    #         self.__post_data(self.__data)
+    #     data: str = self.client.get(f"batch_data/{batch_size}").text
+    #     self.assertEqual(pd.DataFrame(json.loads(data)).shape[0], batch_size)
